e1f3c07c493bdb2cb801ce4074b7acbd1baa8649
Eric Carles <carles.eric@wanadoo.fr>
2017-11-24 15:17:03 +0000

    Comments modified for function add_or_remove_cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -8,7 +8,7 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-#Return void. Add or remove an amount of cash from the total cash amount
+#Add or remove an amount of cash from the total cash amount
 def add_or_remove_cash(pet_shop, cash_amount)
   pet_shop[:admin][:total_cash] += cash_amount
 end
@@ -18,7 +18,7 @@ def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
 
-# Increase the pets sold number of the given pet_sold_number
+#Increase the pets sold number of the given pet_sold_number
 def increase_pets_sold(pet_shop, pet_sold_number)
   pet_shop[:admin][:pets_sold] += pet_sold_number
 end
